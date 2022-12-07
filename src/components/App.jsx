6a93756436631ae72d7839fbde6de86db30f0363
Deepraj <deeprajm35@gmail.com>
2022-12-07 12:11:34 +0530

    Note.jsx modification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../note";
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
